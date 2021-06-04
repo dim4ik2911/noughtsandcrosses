fd35ac8f12fa43c81414d0aa1433e06779a7e61e
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-06-04 15:30:45 +0100

    mobile first and better styling

--- main.js
+++ main.js
@@ -2,18 +2,16 @@
 const squares = document.querySelectorAll(".square");
 
 // winning combinations
-const firstRow = [squares[0], squares[1], squares[2], squares[3]];
-const secondRow = [squares[4], squares[5], squares[6], squares[7]];
-const thirdRow = [squares[8], squares[9], squares[10], squares[11]];
-const fourthRow = [squares[12], squares[13], squares[14], squares[15]];
+const firstRow = [squares[0], squares[1], squares[2]];
+const secondRow = [squares[3], squares[4], squares[5]];
+const thirdRow = [squares[6], squares[7], squares[8]];
 
-const firstColumn = [squares[0], squares[4], squares[8], squares[12]];
-const secondColumn = [squares[1], squares[5], squares[9], squares[13]];
-const thirdColumn = [squares[2], squares[6], squares[10], squares[14]];
-const fourthColumn = [squares[3], squares[7], squares[11], squares[15]];
+const firstColumn = [squares[0], squares[3], squares[6]];
+const secondColumn = [squares[1], squares[4], squares[7]];
+const thirdColumn = [squares[2], squares[5], squares[8]];
 
-const firstDiagonal = [squares[0], squares[5], squares[10], squares[15]];
-const secondDiagonal = [squares[3], squares[6], squares[9], squares[12]];
+const firstDiagonal = [squares[0], squares[4], squares[8]];
+const secondDiagonal = [squares[2], squares[4], squares[6]];
 
 //control turns
 let isPlayerOneTurn = true;
@@ -25,9 +23,8 @@ const checkWinningCondition = (squares) => {
     return square.innerHTML;
   });
   const toCheck = innerHTMLArray.join("");
-  // check IF toCheck equals "xxxx" or "0000"; => return true,  else return false
 
-  if (toCheck === "XXXX" || toCheck === "0000") {
+  if (toCheck === "XXX" || toCheck === "000") {
     return true;
   } else {
     return false;
@@ -41,13 +38,15 @@ for (let index = 0; index < squares.length; index++) {
     }
     if (isPlayerOneTurn) {
       squares[index].innerHTML = "X";
-      squares[index].style.backgroundColor = "red";
+
+      squares[index].style.backgroundColor = "blue";
 
       isPlayerTwoTurn = true;
       isPlayerOneTurn = false;
     } else {
       squares[index].innerHTML = "0";
-      squares[index].style.backgroundColor = "blue";
+
+      squares[index].style.backgroundColor = "red";
 
       isPlayerOneTurn = true;
       isPlayerTwoTurn = false;
@@ -55,12 +54,10 @@ for (let index = 0; index < squares.length; index++) {
     const firstRowWin = checkWinningCondition(firstRow);
     const secondRowWin = checkWinningCondition(secondRow);
     const thirdRowWin = checkWinningCondition(thirdRow);
-    const fourthRowWin = checkWinningCondition(fourthRow);
 
     const firstColumnWin = checkWinningCondition(firstColumn);
     const secondColumnWin = checkWinningCondition(secondColumn);
     const thirdColumnWin = checkWinningCondition(thirdColumn);
-    const fourthColumnWin = checkWinningCondition(fourthColumn);
 
     const firstDiagonalWin = checkWinningCondition(firstDiagonal);
     const secondDiagonalWin = checkWinningCondition(secondDiagonal);
@@ -69,21 +66,19 @@ for (let index = 0; index < squares.length; index++) {
       firstRowWin == true ||
       secondRowWin == true ||
       thirdRowWin == true ||
-      fourthRowWin == true ||
       firstColumnWin == true ||
       secondColumnWin == true ||
       thirdColumnWin == true ||
-      fourthColumnWin == true ||
       firstDiagonalWin == true ||
       secondDiagonalWin == true
     ) {
       if (isPlayerTwoTurn) {
-        result.innerHTML = "PLAYER 1 IS A WINNER";
-        restart.innerHTML = "RESTART";
+        result.innerHTML = "NOUGHTS WON A ROUND";
+        restart.innerHTML = "NEXT ROUND";
         score1.innerHTML++;
       } else {
-        result.innerHTML = "PLAYER 2 IS A WINNER";
-        restart.innerHTML = "RESTART";
+        result.innerHTML = "CROSSES WON A ROUND";
+        restart.innerHTML = "NEXT ROUND";
         score2.innerHTML++;
       }
     }
