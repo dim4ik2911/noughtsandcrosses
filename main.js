6cf201c2d6ca72defac692e0932c87eae78863e6
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-06-05 16:48:14 +0100

    javascript added

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 //declared variables
 const squares = document.querySelectorAll(".square");
+let noughts = document.getElementById("noughts");
+let crosses = document.getElementById("crosses");
+const result = document.getElementById("result");
+const restart = document.getElementById("next-round");
+
+//function to disable restart button
+//function for next round
+
+restart.addEventListener("click", () => {
+  const squaresArray = Array.from(squares);
+  if (restart.innerHTML == "NEXT ROUND") {
+    squaresArray.map((square) => {
+      square.style.backgroundColor = "#23bb49";
+      square.innerHTML = "";
+    });
+    restart.innerHTML = "&nbsp;";
+    result.innerHTML = "&nbsp;";
+  }
+  if (restart.innerHTML == "RESTART A GAME") {
+    squaresArray.map((square) => {
+      square.style.backgroundColor = "#23bb49";
+      square.innerHTML = "";
+    });
+    noughts.innerHTML = 0;
+    crosses.innerHTML = 0;
+    restart.innerHTML = "&nbsp;";
+    result.innerHTML = "&nbsp;";
+  }
+});
 
 // winning combinations
 const firstRow = [squares[0], squares[1], squares[2]];
@@ -31,25 +60,44 @@ const checkWinningCondition = (squares) => {
   }
 };
 
+//checking clicking
 for (let index = 0; index < squares.length; index++) {
   squares[index].addEventListener("click", () => {
     if (squares[index].innerHTML !== "") {
       return; //stops the function
     }
     if (isPlayerOneTurn) {
-      squares[index].innerHTML = "X";
+      if (
+        restart.innerHTML == "RESTART A GAME" ||
+        restart.innerHTML == "NEXT ROUND"
+      ) {
+        squares[index].innerHTML = "&#128540;";
+      } else {
+        result.innerHTML = "&nbsp;";
+        restart.innerHTML = "&nbsp;";
+        squares[index].innerHTML = "X";
 
-      squares[index].style.backgroundColor = "blue";
+        squares[index].style.backgroundColor = "blue";
 
-      isPlayerTwoTurn = true;
-      isPlayerOneTurn = false;
+        isPlayerTwoTurn = true;
+        isPlayerOneTurn = false;
+      }
     } else {
-      squares[index].innerHTML = "0";
+      if (
+        restart.innerHTML == "RESTART A GAME" ||
+        restart.innerHTML == "NEXT ROUND"
+      ) {
+        squares[index].innerHTML = "&#128540;";
+      } else {
+        result.innerHTML = "&nbsp;";
+        restart.innerHTML = "&nbsp;";
+        squares[index].innerHTML = "0";
 
-      squares[index].style.backgroundColor = "red";
+        squares[index].style.backgroundColor = "red";
 
-      isPlayerOneTurn = true;
-      isPlayerTwoTurn = false;
+        isPlayerOneTurn = true;
+        isPlayerTwoTurn = false;
+      }
     }
     const firstRowWin = checkWinningCondition(firstRow);
     const secondRowWin = checkWinningCondition(secondRow);
@@ -73,42 +121,68 @@ for (let index = 0; index < squares.length; index++) {
       secondDiagonalWin == true
     ) {
       if (isPlayerTwoTurn) {
-        result.innerHTML = "NOUGHTS WON A ROUND";
-        restart.innerHTML = "NEXT ROUND";
-        score1.innerHTML++;
+        if (crosses.innerHTML == "&nbsp;" || crosses.innerHTML == 0) {
+          crosses.innerHTML = 1;
+          if (noughts.innerHTML == "&nbsp;") {
+            noughts.innerHTML = 0;
+          }
+
+          result.innerHTML = "CROSSES WON A ROUND";
+          restart.innerHTML = "NEXT ROUND";
+        } else {
+          crosses.innerHTML = parseInt(crosses.innerHTML) + 1;
+          result.innerHTML = "CROSSES WON A ROUND";
+          restart.innerHTML = "NEXT ROUND";
+        }
+        if (crosses.innerHTML == 5) {
+          result.innerHTML = "CROSSES WON A GAME";
+          restart.innerHTML = "RESTART A GAME";
+        }
       } else {
-        result.innerHTML = "CROSSES WON A ROUND";
-        restart.innerHTML = "NEXT ROUND";
-        score2.innerHTML++;
+        if (noughts.innerHTML == "&nbsp;" || noughts.innerHTML == 0) {
+          noughts.innerHTML = 1;
+          if (crosses.innerHTML == "&nbsp;") {
+            crosses.innerHTML = 0;
+          }
+
+          result.innerHTML = "NOUGHTS WON A ROUND";
+          restart.innerHTML = "NEXT ROUND";
+        } else {
+          noughts.innerHTML = parseInt(noughts.innerHTML) + 1;
+          result.innerHTML = "NOUGHTS WON A ROUND";
+          restart.innerHTML = "NEXT ROUND";
+        }
+        if (noughts.innerHTML == 5) {
+          result.innerHTML = "NOUGHTS WON A GAME";
+          restart.innerHTML = "RESTART A GAME";
+        }
       }
     }
   });
 }
 
 //function that restarts the game
-const restart = document.getElementById("next-round");
-const result = document.getElementById("result");
-for (let index = 0; index < squares.length; index++) {
-  restart.addEventListener("click", () => {
-    // if (squares[index].innerHTML == "x" || squares[index].innerHTML == "0") {
-    return (squares[index].innerHTML = "");
-  });
-}
-for (let index = 0; index < squares.length; index++) {
-  restart.addEventListener("click", () => {
-    // if (squares[index].innerHTML == "x" || squares[index].innerHTML == "0") {
-    return (squares[index].style.backgroundColor = "#29bd00");
-  });
-}
-for (let index = 0; index < squares.length; index++) {
-  restart.addEventListener("click", () => {
-    // if (squares[index].innerHTML == "x" || squares[index].innerHTML == "0") {
-    return (result.innerHTML = `&nbsp;`);
-  });
-}
-for (let index = 0; index < squares.length; index++) {
-  restart.addEventListener("click", () => {
-    // if (squares[index].innerHTML == "x" || squares[index].innerHTML == "0") {
-    return (restart.innerHTML = `&nbsp;`);
-  });
-}
+// for (let index = 0; index < squares.length; index++) {
+//   restart.addEventListener("click", () => {
+//     // if (squares[index].innerHTML == "x" || squares[index].innerHTML == "0") {
+//     return (squares[index].innerHTML = "");
+//   });
+// }
+// for (let index = 0; index < squares.length; index++) {
+//   restart.addEventListener("click", () => {
+//     // if (squares[index].innerHTML == "x" || squares[index].innerHTML == "0") {
+//     return (squares[index].style.backgroundColor = "#29bd00");
+//   });
+// }
+// for (let index = 0; index < squares.length; index++) {
+//   restart.addEventListener("click", () => {
+//     // if (squares[index].innerHTML == "x" || squares[index].innerHTML == "0") {
+//     return (result.innerHTML = `&nbsp;`);
+//   });
+// }
+// for (let index = 0; index < squares.length; index++) {
+//   restart.addEventListener("click", () => {
+//     // if (squares[index].innerHTML == "x" || squares[index].innerHTML == "0") {
+//     return (restart.innerHTML = `&nbsp;`);
+//   });
+// }
